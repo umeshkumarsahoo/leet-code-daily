4dfc268e5b34d0534fbf4f705bd032c79172496a
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2024-10-03 19:21:12 +0530

    Time: 411 ms (30.57%), Space: 35 MB (76.09%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.py
@@ -1,17 +1,20 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        total_sum = sum(nums)
-        remainder = total_sum % p
-        if remainder == 0:
-            return 0
-        remainder_map = {0: -1} 
-        prefix_sum = 0
-        min_length = len(nums)
         
-        for i, num in enumerate(nums):
-            prefix_sum = (prefix_sum + num) % p
-            target_remainder = (prefix_sum - remainder) % p 
-            if target_remainder in remainder_map:
-                min_length = min(min_length, i - remainder_map[target_remainder])
-            remainder_map[prefix_sum] = i
-        return min_length if min_length < len(nums) else -1
+        target = sum(nums) % p
+
+        if target == 0:
+            return 0
+        
+        presum = {0 : -1}
+        total = 0
+        res = len(nums)
+        for i in range(len(nums)):
+            total += nums[i]
+            mod = (total - target) % p
+            if mod in presum:
+                res = min(res, i - presum[mod])
+            presum[total%p] = i 
+        
+        return res if res != len(nums) else -1
+        
