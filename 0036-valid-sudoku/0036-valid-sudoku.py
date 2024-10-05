467d00e73896a5e57839fd6f6eb65aa63d32e030
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2024-10-05 17:48:01 +0530

    Time: 47 ms (99.75%), Space: 16.5 MB (88.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,27 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
+    def isValidSudoku(board: List[List[str]]) -> bool:
         cols = defaultdict(set)
-        boxes = defaultdict(set)
-        
+        row = defaultdict(set)
+        sq = defaultdict(set)
+
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                cell = board[r][c]
+
+                if cell == '.':
                     continue
-                
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r // 3, c // 3)]:
+
+                if(cell in row[r] or cell in cols[c] or cell in sq[(r//3, c//3)]):
+
                     return False
-                
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                boxes[(r // 3, c // 3)].add(board[r][c])
-        
+                else:
+                    row[r].add(cell)
+                    cols[c].add(cell)
+                    sq[(r//3, c//3)].add(cell)
+
         return True
+
+    with open('user.out', 'w') as f:
+        for case in map(loads, stdin):
+            f.write(f'{str(isValidSudoku(case)).lower()}\n')
+    exit()
