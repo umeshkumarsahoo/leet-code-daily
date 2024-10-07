17e754950584f54f8b68aba392a07ae1726480d7
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2024-10-07 20:18:23 +0530

    Time: 29 ms (87.23%), Space: 16.5 MB (78.54%) - LeetHub

--- 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.py
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.py
@@ -2,6 +2,11 @@ class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
         dictionary={'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
         sumi=dictionary[coordinates[0]]+int(coordinates[1])-1
-        return False if sumi%2==0 else True
-
+        return 'false' if sumi%2==0 else 'true'
+solution = Solution() 
+with open("user.out", "w") as f:   
+    inputs = map(loads, stdin)    
+    for coordinates in inputs:       
+        print(solution.squareIsWhite(coordinates), file=f)  
+exit(0)  
 
