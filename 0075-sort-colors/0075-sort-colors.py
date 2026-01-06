570c0aa163c474980826666d8184a52dc7dd92a3
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-01-06 20:18:28 +0530

    Time: 0 ms (100%), Space: 19.4 MB (8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,9 +4,25 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
-        for i in range(n):
-            for j in range(0,n-i-1):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
+        start=0
+        end=n-1
+        mid=0
+        while mid<=end:
+            if (nums[mid]==0):
+                nums[mid],nums[start]=nums[start],nums[mid]
+                mid+=1
+                start+=1
+            elif(nums[mid]==1):
+                mid+=1
+            else:
+                nums[end],nums[mid]=nums[mid],nums[end]
+                end-=1
+
+        
+
+        # for i in range(n):
+        #     for j in range(0,n-i-1):
+        #         if nums[j]>nums[j+1]:
+        #             nums[j],nums[j+1]=nums[j+1],nums[j]
         
         
