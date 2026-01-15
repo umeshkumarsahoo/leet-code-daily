fb8e38537805f9617b2c6efbddd430aa8ceaede5
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-01-15 18:35:08 +0530

    Time: 3 ms (34.8%), Space: 22.5 MB (6.06%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,19 +1,18 @@
-# Definition for a binary tree node.  
-# class TreeNode:  
-#     def __init__(self, val=0, left=None, right=None):  
-#         self.val = val  
-#         self.left = left  
-#         self.right = right  
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        ans=[]
+        def inorder(node):
+            if  node:
+                inorder(node.left)
+                ans.append(node.val)                
+                inorder(node.right)
+        inorder(root)
+        return ans[k-1] if k<=len(ans) else None
 
-class Solution:  
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:  
-        ans = []  
         
-        def inorder(node):  
-            if node:  
-                inorder(node.left)  
-                ans.append(node.val)  
-                inorder(node.right)  
-        
-        inorder(root)  
-        return ans[k - 1] if k <= len(ans) else None
