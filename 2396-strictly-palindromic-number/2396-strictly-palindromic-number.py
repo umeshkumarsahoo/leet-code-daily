2fde254931261c6cf8c06acd643f25d9b001c81d
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-02-21 23:44:12 +0530

    Time: 0 ms (100%), Space: 19.4 MB (14.31%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
+        
