522a06239971af54e5fa71af0d3c68288452c13e
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-06-03 03:37:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (66.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,18 +1,19 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
-        Modify nums in-place to find the next lexicographical permutation.
+        Do not return anything, modify nums in-place instead.
         """
-        g_idx = -1
-        for i in range(len(nums) - 1, 0, -1):
-            if nums[i] > nums[i - 1]:
-                g_idx = i - 1
+        g_idx=-1
+        size=len(nums)
+        for i in range(size-1,0,-1):
+            if nums[i]>nums[i-1]:
+                g_idx=i-1
                 break
-        if g_idx != -1:
-            swap_idx=i
-            for j in range(len(nums)-1,g_idx,-1):
-                if nums[j] > nums[g_idx]:
-                    swap_idx=j
+        if g_idx!=-1:
+            for i in range(size-1,g_idx,-1):
+                if nums[i]>nums[g_idx]:
+                    nums[i],nums[g_idx]=nums[g_idx],nums[i]
                     break
-            nums[swap_idx], nums[g_idx] = nums[g_idx], nums[swap_idx]
-        nums[g_idx + 1:] = nums[g_idx + 1:][::-1]
+        nums[g_idx+1:]=nums[g_idx+1:][::-1]
+        return nums
+                
