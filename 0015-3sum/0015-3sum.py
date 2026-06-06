94ea84f638481be39d82fa0179fc8994bd299436
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-06-06 13:41:17 +0530

    Time: 867 ms (20.95%), Space: 22.1 MB (81.18%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,25 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        nums = sorted(nums)
-        res = []
+        nums.sort()
+        res=[]
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-
-            near, far = i + 1, len(nums) - 1
-            while near < far:
-                total = nums[i] + nums[near] + nums[far]
-                if total == 0:
-                    res.append([nums[i], nums[near], nums[far]])
-                    while near < far and nums[near] == nums[near + 1]:
-                        near += 1
-                    while near < far and nums[far] == nums[far - 1]:
-                        far -= 1
-                    near += 1
-                    far -= 1
-                elif total < 0:
-                    near += 1
+            near,far=i+1,len(nums)-1
+            while near<far:
+                if nums[near]+nums[far]+nums[i]==0:
+                    res.append([nums[near],nums[far],nums[i]])
+                    while near<far and nums[near]==nums[near+1]:
+                        near+=1
+                    while near<far and nums[far]==nums[far-1]:
+                        far-=1
+                    near+=1
+                    far-=1
+                elif nums[near]+nums[far]+nums[i]>0:
+                    far-=1
                 else:
-                    far -= 1
+                    near+=1
         return res
+
+
+        
