c0cfbf6396b0d1cc5e5ad4e5bb7ed905751e15b9
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-06-08 20:57:27 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.14%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -1,7 +1,7 @@
 from math import ceil
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
-        time=-(-len(b)//len(a))
+        time=ceil(len(b)/len(a))
         temp=time*a
         if b in temp: return time
         if b in temp+a: return time+1
