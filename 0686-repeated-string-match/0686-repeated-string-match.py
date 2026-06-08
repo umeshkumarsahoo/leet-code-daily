57da34a8697bbc11a7716af3291ee9489489cd9e
Umesh kumar sahoo <88975638+umeshkumarsahoo@users.noreply.github.com>
2026-06-08 20:54:41 +0530

    Time: 0 ms (100%), Space: 19.1 MB (96.04%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -0,0 +1,9 @@
+from math import ceil
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        time=-(-len(b)//len(a))
+        temp=time*a
+        if b in temp: return time
+        if b in temp+a: return time+1
+        return -1
+        
